67a6ba41b7ca4eac3a40a4526a7a234a0cbe4d68
killeen31 <j31killeen@gmail.com>
2023-03-23 18:07:36 -0500

    questions array and markdown function

--- README.md
+++
@@ -1,2 +0,0 @@
-# KilleenMe-ReadMe-Generator
-This is a command line application that creates a generated ReadMe file
--- index.js
+++ index.js
@@ -1,27 +1,125 @@
 // TODO: Include packages needed for this application
 
-const { default: inquirer } = require("inquirer");
+const inquirer = require("inquirer");
+let fs = require('fs')
+let util = require('util')
+// const generMarkdown = require('./utils/generateMarkdown')
+
+let writeFileAsync = util.promisify(fs.writeFile)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Tell users how to install app',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How is this app used?',
+        },
+        {
+            type: 'checkbox',
+            name: 'liscence',
+            message: 'Which lisence are you using for this project?',
+            choices: ['MIT', 'ISC', 'Apache', 'GNU GPLv3']
+        },
+        {
+            type: 'input',
+            message: 'How do others contribute to this project?',
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: 'Whose credit is this work?',
+            name: 'credits'
+        },
+        {
+            type: 'input',
+            message: 'Test instructions',
+            name: 'test'
+        },
+        {
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        },
+    ])
 
-inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        
-    }
-])
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function createMarkDown(response) { 
+    return`
+    # ${response.title}
+
+    # Table of Contents 
+
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Test](#test)
+    - [Credits](#credits)
+    - [Liscence](#liscence)
+    - [Questions](#questions)
+
+    ## Decription 
+    ${response.description}
+
+    ## Installation
+    `
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const response = await promptUser()
+        const readMe = createMarkDown(response)
+
+        await writeFileAsync('README.md', readMe)
+        console.log ('Success!')
+    } catch (err){
+        console.log ('there was an error!', err)
+    }
+}
+
+init()
+
+
+
+// function init() {
+//     inquirer
+//         .prompt(questions)
+//         .then((answers) => {
+//             console.log(answers)
+//             console.log(answers.anyWord)
+//             console.log(answers.listItem)
+//             console.log(project)
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
+// }
 
 // Function call to initialize app
-init();
+
